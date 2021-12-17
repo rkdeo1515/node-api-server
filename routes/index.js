089a83d6bbe8405b557698dcdadf5abb1d37f31d
GangDae Ju <rkdeo1515@gmail.com>
2021-12-17 17:01:57 +0900

    211217

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+var info = {
+  id: '0x00',
+  rssi: 'rssi_val',
+  temp: 'temp_val',
+  hum: 'hum_val',
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+  res.send(JSON.stringify(info));
+});
+
+router.get('/id', function(req, res, next) {
+  res.send(info.id);
+});
+
+router.get('/rssi', function(req, res, next) {
+  res.send(info.rssi);
+});
+
+router.get('/temp', function(req, res, next) {
+  res.send(info.temp);
+});
+
+router.get('/hum_val', function(req, res, next) {
+  res.send(info.hum);
 });
 
 module.exports = router;
